e733b17ce57f89baa8636d069f9651738349ea61
Vladislav Gaidenko <90133855+vladjong@users.noreply.github.com>
2022-07-17 18:27:01 +0700

    Update and rename set_matrix_zero_constant to set_matrix_zero_constant.cpp

--- beardedAlgorithm/set_matrix_zero_constant.cpp
+++ beardedAlgorithm/set_matrix_zero_constant.cpp
@@ -1,26 +1,22 @@
 class Solution
 {
 public:
-  void setZeroes(vector<vector<int>>& matrix)
-  {
-    bool FirstColumn = false;
-    bool FirstRow = false;
+  void setZeroes(vector<vector<int>>& matrix) {
+    bool fCol = false;
+    bool fRow = false;
     int i, j;
- 
     for(i = 0; i < matrix.size(); i++)
       if(matrix[i][0] == 0)
       {
-        FirstColumn = true;
+        fCol = true;
         break;
       }
- 
     for(i = 0; i < matrix[0].size(); i++)
       if(matrix[0][i] == 0)
       {
-        FirstRow = true;
+        fRow = true;
         break;
       }
- 
     for(i = 1; i < matrix.size(); i++)
     for(j = 1; j < matrix[0].size(); j++)
       if(matrix[i][j] == 0)
@@ -28,16 +24,13 @@ public:
         matrix[i][0] = 0;
         matrix[0][j] = 0;
       }
- 
     for(i = 1; i < matrix.size(); i++)
     for(j = 1; j < matrix[0].size(); j++)
       if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
- 
-    if(FirstColumn)
+    if(fCol)
       for(i = 0; i < matrix.size(); i++)
         matrix[i][0] = 0;
- 
-    if(FirstRow)
+    if(fRow)
       for(i = 0; i < matrix[0].size(); i++)
         matrix[0][i] = 0;
   }
