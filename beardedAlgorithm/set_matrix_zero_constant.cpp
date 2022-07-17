76de1b39300f8c580ca2b30e86323cd57f639651
Vladislav Gaidenko <90133855+vladjong@users.noreply.github.com>
2022-07-17 18:37:43 +0700

    Update set_matrix_zero_constant.cpp

--- beardedAlgorithm/set_matrix_zero_constant.cpp
+++ beardedAlgorithm/set_matrix_zero_constant.cpp
@@ -6,21 +6,18 @@ public:
     bool fRow = false;
     int i, j;
     for(i = 0; i < matrix.size(); i++)
-      if(matrix[i][0] == 0)
-      {
+      if(matrix[i][0] == 0) {
         fCol = true;
         break;
       }
     for(i = 0; i < matrix[0].size(); i++)
-      if(matrix[0][i] == 0)
-      {
+      if(matrix[0][i] == 0) {
         fRow = true;
         break;
       }
     for(i = 1; i < matrix.size(); i++)
     for(j = 1; j < matrix[0].size(); j++)
-      if(matrix[i][j] == 0)
-      {
+      if(matrix[i][j] == 0 {
         matrix[i][0] = 0;
         matrix[0][j] = 0;
       }
