6e231ce807302048b2f749b9e8e9a62f42b0df6d
Vladislav Gaidenko <90133855+vladjong@users.noreply.github.com>
2023-05-19 12:04:22 +0700

    Update сontains_duplicate_II_219.go

--- beardedAlgorithm/сontains_duplicate_II_219.go
+++ beardedAlgorithm/сontains_duplicate_II_219.go
@@ -1,14 +1,13 @@
 func containsNearbyDuplicate(nums []int, k int) bool {
-    unique := make(map[int]int)
+    unique := make(map[int]struct{})
     for i, val := range nums {
-        if p, ok := unique[val]; ok {
-            if k >= int(math.Abs(float64(p - i))) {
-                return true
-            }
-            unique[val] = i
-            continue
+        if _, ok := unique[val]; ok {
+            return true
         }
-        unique[val] = i
+        unique[val] = struct{}{}
+        if len(unique) > k {
+            delete(unique, nums[i-k])
+        }
     }
     return false
 }
